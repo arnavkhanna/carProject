cc69cfd80228e8f852fae816a0e94dd3c572bc97
arnavkhanna <arnavkhanna@gmail.com>
2019-07-01 12:38:03 -0700

    initial commit

--- src/main/java/com/car/repositories/CarRepository.java
+++
@@ -1,7 +0,0 @@
-package com.car.repositories;
-
-import com.car.entity.Car;
-import org.springframework.data.repository.PagingAndSortingRepository;
-
-public interface CarRepository extends PagingAndSortingRepository<Car, String> {
-}
